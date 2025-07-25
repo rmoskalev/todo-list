31cd891529c55d837048408bf00383d0b1a0df75
Ruslan <rmoskalevdev@gmail.com>
2025-07-25 03:21:04 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-gray-100">
+			<h1 className="text-3xl font-bold text-blue-600">
+				To Do List на Vite + React + Tailwind + TS
+			</h1>
+		</div>
+	);
+}
+
+export default App;
